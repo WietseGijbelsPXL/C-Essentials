29067a9181e50d4496ad1bd0f719a0bd3c5ce757
Wietse-Gijbels <wietse.gijbels@student.ucll.be>
2025-12-10 15:22:19 +0100

    next

---
+++ Registratie/Registratie/Models/Students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registratie.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public char Sex { get; set; }
+        public List<Olod> Olods { get; set; }
+
+        public Student()
+        {
+            this.Olods = new List<Olod>();
+        }
+
+        public string GetOlodSummary()
+        {
+            return string.Join(",", this.Olods.Select(o => o.Name));
+        }
+
+        private int GetTotalCredits()
+        {
+            return this.Olods.Sum(o => o.Credits);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} ({this.Sex})\r\n{this.GetTotalCredits()} studiepunten";
+        }
+    }
+}
---
+++ WPFDemo2/WPFDemo2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDemo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (subscribecCheckBox.IsChecked == true)
+            {
+                infoLabel.Content = "aangevinkt";
+            }
+
+            
+        }
+
+        private void subscribecCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            infoLabel.Content = "aangevinkt";
+        }
+
+        private void subscribecCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            infoLabel.Content = "uitgevinkt";
+        }
+    }
+}
